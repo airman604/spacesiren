7b74fa51b8a1487518fe3a233df2c4bc72c6e75a
Airman604 <4887927+airman604@users.noreply.github.com>
2025-02-01 20:07:23 -0600

    fix: convert version constraints to >=

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -2,20 +2,20 @@
 # Copy that file to terraform-local.tf.
 
 terraform {
-  required_version = "~> 1"
+  required_version = ">= 1.1.2"
 
   required_providers {
     archive = {
       source  = "hashicorp/archive"
-      version = "~> 2.2.0"
+      version = ">= 2.2.0"
     }
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.70.0"
+      version = ">= 3.70.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.1.0"
+      version = ">= 3.1.0"
     }
   }
 }
